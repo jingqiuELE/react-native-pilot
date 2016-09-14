cd2f17315366e228048fb0beb0ddcc3c993771ee
jingqiuELE <aqiu0720@gmail.com>
2016-09-14 13:17:48 +0800

    Implement background picture.

--- index.android.js
+++ index.android.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
 
 class helloworld extends Component {
       render() {
           return (
-              <Text>Hello world!</Text>
+            <View style={{ flex: 1 }}>
+              <Image source={require('./image.jpg')} style={styles.backgroundImage}>
+                <Text style={styles.red}>
+                  Hello world!
+                </Text>
+              </Image> 
+            </View>
           );
       }
 }
+
+const styles = StyleSheet.create({
+      backgroundImage: {
+          flex: 1,
+          resizeMode: 'stretch',
+          width: null,
+          height: null,
+      },
+      red: {
+          color: 'red',
+          textAlign: 'center',
+      },
+});
 
 AppRegistry.registerComponent('helloworld', () => helloworld);
