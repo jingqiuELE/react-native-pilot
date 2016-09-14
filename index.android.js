6a3f2da149ae9357701d1b0f857ba992a431acb9
jingqiuELE <aqiu0720@gmail.com>
2016-09-14 20:54:36 +0800

    Implement View Change on socket.io event.

--- index.android.js
+++ index.android.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
+import './UserAgent';
+import io from 'socket.io-client/socket.io';
 import { AppRegistry, StyleSheet, Text, View, Image } from 'react-native';
 
 class helloworld extends Component {
-      render() {
-          return (
-            <View style={{ flex: 1 }}>
-              <Image source={require('./image.jpg')} style={styles.backgroundImage}>
+    constructor(props) {
+        super(props);
+        this.state = {picId: 0, chat: "Hello World!"};
+
+        this.socket = io('http://10.0.2.2:3000', {jsonp: false});
+        this.socket.on('chat message', function(msg){
+            console.log('chat message:' + msg);
+            console.log('typeof picId' + typeof(this.state.picId));
+            let picId = 1 - this.state.picId;
+            this.setState({picId: picId, chat: msg})
+        }.bind(this));
+    }
+
+    render() {
+        return <ViewPicture pic={this.state.picId} chat={this.state.chat} />;
+    }
+}
+
+class ViewPicture extends Component {
+    render() {
+        if (this.props.pic == 1) {
+            var pic = require('./images/image1.jpg');
+        } else {
+            var pic = require('./images/image0.jpg');
+        } 
+        return (
+            <View style={{flex: 1}}>
+              <Image source={pic} style={styles.backgroundImage}>
                 <Text style={styles.red}>
-                  Hello world!
+                   {this.props.chat} 
                 </Text>
               </Image> 
             </View>
-          );
-      }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-      backgroundImage: {
-          flex: 1,
-          resizeMode: 'stretch',
-          width: null,
-          height: null,
-      },
-      red: {
-          color: 'red',
-          textAlign: 'center',
-      },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'stretch',
+        width: null,
+        height: null,
+    },
+    red: {
+        color: 'red',
+        textAlign: 'center',
+    },
 });
 
 AppRegistry.registerComponent('helloworld', () => helloworld);
